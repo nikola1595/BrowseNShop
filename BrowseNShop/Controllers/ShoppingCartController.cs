2bba51505bbc794cd2c80f4db3f0f8d35e83bd4a
nikola <nilic1595@gmail.com>
2019-10-29 11:26:06 +0100

    Adding Shopping Cart Controller With Fs

---
+++ BrowseNShop/Controllers/ShoppingCartController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BrowseNShop.Data.Models;
+using BrowseNShop.Interfaces;
+using BrowseNShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BrowseNShop.Controllers
+{
+
+    public class ShoppingCartController : Controller
+    {
+        private readonly ISneakerRepository _sneakerRepository;
+        private readonly ShoppingCart _shoppingCart;
+
+        public ShoppingCartController(ISneakerRepository sneakerRepository, ShoppingCart shoppingCart)
+        {
+            _sneakerRepository = sneakerRepository;
+            _shoppingCart = shoppingCart;
+
+        }
+
+
+        public ViewResult Index()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+
+
+            var SCVM = new ShoppingCartViewModel
+            {
+                ShoppingCart = _shoppingCart,
+                ShoppingCartTotal = _shoppingCart.GetShoppingTotal()
+
+            };
+
+
+            return View(SCVM);
+        }
+
+
+
+        public RedirectToActionResult AddToShoppingCart(int sneakerID)
+        {
+            var selectedSneaker = _sneakerRepository.Sneakers.FirstOrDefault(f => f.SneakerID == sneakerID);
+
+            if (selectedSneaker != null)
+            {
+                _shoppingCart.AddToCart(selectedSneaker, 1);
+
+            }
+            return RedirectToAction("Index");
+        }
+
+
+
+        public RedirectToActionResult RemoveFromShoppingCart(int sneakerID)
+        {
+            var selectedSneaker = _sneakerRepository.Sneakers.FirstOrDefault(f => f.SneakerID == sneakerID);
+
+            if(selectedSneaker != null)
+            {
+                _shoppingCart.RemoveFromCart(selectedSneaker);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        
+    }
+}
