005671fedd9c4d0582063b8a098db16c1b7307dd
nikola <nilic1595@gmail.com>
2019-11-04 17:54:01 +0100

    Adding Order Repository

---
+++ BrowseNShop/Data/Repositories/OrderRepository.cs
@@ -0,0 +1,45 @@
+﻿using BrowseNShop.Data.Interfaces;
+using BrowseNShop.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BrowseNShop.Data.Repositories
+{
+    public class OrderRepository:IOrderRepository
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ShoppingCart _shoppingCart;
+
+        public OrderRepository(ApplicationDbContext context, ShoppingCart shoppingCart)
+        {
+            _context = context;
+            _shoppingCart = shoppingCart;
+        }
+
+        public void CreateOrder(Order order)
+        {
+            order.OrderPlaced = DateTime.Now;
+            _context.Orders.Add(order);
+
+            var shoppingCartItems = _shoppingCart.ShoppingCartItems;
+
+            foreach(var item in shoppingCartItems)
+            {
+                var orderDetail = new OrderDetail()
+                {
+                    Amount = item.Amount,
+                    SneakerID = item.Sneaker.SneakerID,
+                    OrderID = order.OrderID,
+                    Price = item.Sneaker.Price,
+                };
+
+                _context.OrderDetails.Add(orderDetail);
+
+                
+            }
+            _context.SaveChanges();
+        }
+    }
+}
