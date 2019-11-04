e1efa6744f879766b525ccb266580b0a024f6192
nikola <nilic1595@gmail.com>
2019-11-04 20:33:52 +0100

    Order Model Validation

--- BrowseNShop/Controllers/OrderController.cs
+++ BrowseNShop/Controllers/OrderController.cs
@@ -28,6 +28,33 @@ namespace BrowseNShop.Controllers
             return View();
         }
 
+        [HttpPost]
+        public IActionResult Checkout(Order order)
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+            if (_shoppingCart.ShoppingCartItems.Count == 0)
+            {
+                ModelState.AddModelError("", "Your cart is empty.Add some products to your cart");
+            }
+            if (ModelState.IsValid)
+            {
+                _orderRepository.CreateOrder(order);
+                _shoppingCart.ClearCart();
+                return RedirectToAction("CheckoutComplete");
+            }
+
+
+            return View(order);
+
+        }
+
+        public IActionResult CheckoutComplete()
+        {
+            ViewBag.CheckoutCompleteMessage = "Thanks for your order!";
+            return View();
+        }
+
 
 
     }
--- BrowseNShop/Data/Models/Order.cs
+++ BrowseNShop/Data/Models/Order.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,28 +9,59 @@ namespace BrowseNShop.Data.Models
 {
     public class Order
     {
+        [BindNever]
         public int OrderID { get; set; }
 
         public List<OrderDetail> OrderDetails { get; set; }
 
 
-
+        [Display(Name ="First Name")]
+        [StringLength(50)]
+        [Required(ErrorMessage ="Please enter your first name")]
         public string FirstName { get; set; }
 
+
+        [Display(Name = "Last Name")]
+        [StringLength(50)]
+        [Required(ErrorMessage = "Please enter your last name")]
         public string LastName { get; set; }
 
+        [Display(Name = "Address Line")]
+        [StringLength(100)]
+        [Required(ErrorMessage = "Please enter your address")]
+        public string AddressLine { get; set; }
+
+        [Required(ErrorMessage = "Please enter your zip code")]
+        [Display(Name = "Zip code")]
+        [StringLength(10, MinimumLength = 4)]
         public string ZipCode { get; set; }
 
+        [StringLength(10)]
         public string State { get; set; }
 
+        [Required(ErrorMessage = "Please enter your country")]
+        [StringLength(50)]
         public string Country { get; set; }
 
+        [Required(ErrorMessage = "Please enter your phone number")]
+        [StringLength(25)]
+        [DataType(DataType.PhoneNumber)]
+        [Display(Name = "Phone number")]
         public string PhoneNumber { get; set; }
 
+        [Required]
+        [StringLength(50)]
+        [DataType(DataType.EmailAddress)]
+        [RegularExpression(@"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|""(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*"")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])",
+           ErrorMessage = "The email address is not entered in a correct format")]
         public string Email { get; set; }
 
+        [BindNever]
+        [ScaffoldColumn(false)]
         public decimal OrderTotal { get; set; }
 
+        [BindNever]
+        [ScaffoldColumn(false)]
         public DateTime OrderPlaced { get; set; }
 
 
